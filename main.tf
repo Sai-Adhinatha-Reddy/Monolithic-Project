97cfc84374bf548f9694829d3d6f73478a15f6c4
Sai-Adhinatha-Reddy <91363558+Sai-Adhinatha-Reddy@users.noreply.github.com>
2025-01-27 11:32:11 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,9 +1,9 @@
 resource "aws_launch_template" "web_server_as" {
     name = "myproject"
-    image_id           = "ami-0454e52560c7f5c55"
+    image_id           = "ami-0f214d1b3d031dc53"
     vpc_security_group_ids = [aws_security_group.web_server.id]
     instance_type = "t2.micro"
-    key_name = "lastone"
+    key_name = "Adhi@2209"
     tags = {
         Name = "DevOps"
     }
@@ -15,7 +15,7 @@ resource "aws_launch_template" "web_server_as" {
   resource "aws_elb" "web_server_lb"{
      name = "web-server-lb"
      security_groups = [aws_security_group.web_server.id]
-     subnets = ["subnet-024286466e258f339", "subnet-09558309acf4661a8"]
+     subnets = ["subnet-0f127e1514b26eae6", "subnet-050427cc8d8392cc1"]
      listener {
       instance_port     = 8000
       instance_protocol = "http"

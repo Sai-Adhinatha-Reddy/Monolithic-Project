2e7a913fb7a4e39805e5ba9a4d167c5bd391a201
Sai-Adhinatha-Reddy <91363558+Sai-Adhinatha-Reddy@users.noreply.github.com>
2025-01-27 14:21:52 -0600

    Update security.tf

--- security.tf
+++ security.tf
@@ -20,6 +20,13 @@ resource "aws_security_group" "web_server" {
      cidr_blocks = ["0.0.0.0/0"]
    }
 
+  ingress {
+     from_port   = 0
+     to_port     = 0
+     protocol    = "-1"
+     cidr_blocks = ["0.0.0.0/0"]
+   }
+
   egress {
      from_port   = 0
      to_port     = 0
